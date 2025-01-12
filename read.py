631bc1c3fe957214b25599be1fbd3c0a58af3675
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2025-01-12 10:49:29 +0530

    PROCESSS

--- read.py
+++ read.py
@@ -4,6 +4,14 @@ img = cv.imread('Photos/cat_large.jpg')
 
 cv.imshow('cat' , img)
 
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width ,height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+                     
+
 cv.waitKey(0)
 
 
