9a569a3aedc9a4af18e5654fc559d4498feeff79
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2025-01-13 19:43:05 +0530

    process

--- draw.py
+++ draw.py
@@ -4,7 +4,11 @@ import numpy as np
 blank = np.zeros((500,500,3) , dtype='uint8')
 cv.imshow('Blank', blank)
 
-blank[:] = 0,255,0
+#point the image certain colour
+blank[:] = 0,0,255
 cv.imshow('Green', blank)
 
+#2. Draw a ractangle
+cv.rectangle(blank, (0,0) ,(250,250) , (0,255,0), thickness=2)
+cv.imshow('Rectangle' ,blank)
 cv.waitKey(0)
